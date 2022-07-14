cdfdf22f77314fbcbb80c6568c27ad57925174b5
SSAFY <SSAFY@DESKTOP>
2022-07-14 17:47:43 +0900

    Day 1 - 자바 기초(데이터 타입, 연산자, 조건문, 반복문) 2022-07-14

--- lab/workspace/work_java/src/week1/day_0714/LoopTest2.java
+++ lab/workspace/work_java/src/week1/day_0714/LoopTest2.java
@@ -1,22 +1,24 @@
 package week1.day_0714;
 
-public class LoopTest {
+public class LoopTest2 {
 
 	public static void main(String[] args) {
 		
-		// 1 ~ 10 까지의 누적 합 구하기
-		int sum = 0;
+		// 1 ~ 10 까지의 홀수의 누적합 구하기 continue 활용
 		// for 문
+		int sum = 0;
 		for(int i=1; i<=10; i++) {
+			if(i % 2 == 0) continue;
 			sum += i;
 		}
 		System.out.println(sum);
 		sum = 0;
 		
 		// while 문
-		int j = 1;
-		while(j <= 10) {
-			sum += j++;
+		int j = 0;
+		while(++j < 10) {
+			if(j % 2 == 0) continue;
+			sum += j;
 		}
 		System.out.println(sum);
 
