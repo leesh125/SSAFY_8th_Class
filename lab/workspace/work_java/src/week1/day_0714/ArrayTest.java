3ce294aaf93ee2edf09c09f78fb18ea50aa585da
leesh125 <chelsea12345@naver.com>
2022-07-19 10:19:21 +0900

    modify ArrayTest.java print

---
+++ lab/workspace/work_java/src/week1/day_0714/ArrayTest.java
@@ -0,0 +1,14 @@
+package week1.day_0714;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3};
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
