633df45870ce3d484ea99a92b2ad1d0a8cc3a7eb
leesh125 <chelsea12345@naver.com>
2022-08-24 22:06:14 +0900

    Day 27 과제

--- lab/workspace/work_python/Day_0824/Main_16236_아기상어_이석현.py
+++ lab/workspace/work_python/Day_0824/Main_16236_아기상어_이석현.py
@@ -1,13 +1,14 @@
 import sys
+from collections import deque
 
-def find_smaller_dis():
+def find_smaller_dis(fishes):
     temp = []
     for fish in fishes:
-        x,y,size = fish
-        if size >= shark[2]:
+        x,y,fish_dis = fish
+        if graph[x][y] >= shark[2]:
             continue
         else:
-            dis = abs(shark[0] - x) + abs(shark[1] - y)
+            dis = fish_dis
             if len(temp) == 0:
                 temp = [x,y]
                 min_dis = dis
@@ -26,11 +27,34 @@ def find_smaller_dis():
     else:
         return temp, min_dis
 
+def bfs(i,j,sharksize):
+    q = deque()
+    visited = [[False] * N for _ in range(N)]
+    visited[i][j] = True
+    q.append((i,j,0))
+    fishes = []
+    while q:
+        x, y, cnt = q.popleft()
+
+        for dir in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nx = x + dir[0]
+            ny = y + dir[1]
+
+            if 0<=nx<N and 0<=ny<N and not visited[nx][ny]:
+                if graph[nx][ny] == 0: 
+                    visited[nx][ny] = True
+                    q.append((nx,ny,cnt+1))
+                elif graph[nx][ny] <= sharksize:
+                    fishes.append([nx,ny,cnt+1])
+                    visited[nx][ny] = True
+                    q.append((nx,ny,cnt+1))
+    return fishes
+
+
 input = sys.stdin.readline
 N = int(input())
 graph = []
 shark = []
-fishes = []
 ans = 0
 eat = 0
 
@@ -40,16 +64,19 @@ for i in range(N):
     for j in range(N):
         if temp[j] == 9:
             shark = [i,j,2]
-        elif 1<=temp[j]<=6:
-            fishes.append([i,j,temp[j]])
 
 while True:
-    kill_fish, dis = find_smaller_dis()
+    fishes = bfs(shark[0], shark[1], shark[2])
+    kill_fish, dis = find_smaller_dis(fishes)
     if dis == -1: break
-    fishes.remove(kill_fish)
+    fishes.remove(kill_fish + [dis])
+    graph[shark[0]][shark[1]] = 0
+    shark[0] = kill_fish[0]; shark[1] = kill_fish[1]
+    graph[kill_fish[0]][kill_fish[1]] = 0
     eat += 1
     if eat == shark[2]:
         shark[2] += 1
         eat = 0
     ans += dis
+
 print(ans)
