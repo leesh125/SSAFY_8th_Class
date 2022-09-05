2faeb541e25edc0bdea8a440338130fc088b6bb4
leesh125 <chelsea12345@naver.com>
2022-09-06 03:31:17 +0900

    Day 33 Javascript / JSON, LocalStorage
    2022-09-01

--- lab/workspace/work_frontend/JavaScript/frontest/js/poll.js
+++ lab/workspace/work_frontend/JavaScript/frontest/js/poll.js
@@ -1,3 +1,61 @@
 window.onload = function () {
-  
+  document.querySelector("#btn-add").addEventListener("click", function() {
+    let listDiv = document.querySelector("#poll-answer-list")
+
+    let divEl = document.createElement("div");
+    divEl.setAttribute("class", "poll-answer-item");
+
+    let inputEl = document.createElement("input");
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("name", "answer");
+
+    let buttonEl = document.createElement("button");
+    buttonEl.setAttribute("type", "button");
+    buttonEl.setAttribute("class", "button");
+    buttonEl.appendChild(document.createTextNode("삭제"));
+
+    buttonEl.addEventListener("click", function() {
+        let parentEl = this.parentElement;
+        listDiv.removeChild(parentEl);
+
+    });
+
+    divEl.appendChild(inputEl);
+    divEl.appendChild(buttonEl);
+
+    listDiv.appendChild(divEl);
+  });
+
+  document.querySelector("#btn-make").addEventListener("click", function() {
+    let question = document.querySelector("#question").value;
+    // console.log(question);
+    if(!question){
+        alert("질문은 반드시 입력!!");
+        return;
+    }
+
+    let answers = document.querySelectorAll("[name=answer]");
+    console.log(answers);
+    for(let answer of answers){
+        // console.log(answer.value);
+        if(!answer.value){
+            alert("답변을 입력!!");
+            return;
+        }
+    }
+
+    let answerArr = [];
+    for(let answer of answers){
+        answerArr.push(answer.value); // ["서울", ..., "부울경"]
+    }
+
+    localStorage.setItem("question",question);
+    localStorage.setItem("answers", JSON.stringify(answerArr)); // 배열이 문자열 형태로 저장됨
+
+    if(confirm("투표생성하시겠습니까??")){
+        opener.location.reload(); // opener : 나 자신을 연 창
+        self.close();
+    }
+
+  });
 };
