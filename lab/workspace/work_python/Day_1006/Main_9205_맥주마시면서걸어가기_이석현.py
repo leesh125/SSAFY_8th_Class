4b44e192b1bea262957479180b91968b9d0bbf61
leesh125 <chelsea12345@naver.com>
2022-10-07 18:03:01 +0900

    맥주 과제 1006과제

--- lab/workspace/work_python/Day_1006/Main_9205_맥주마시면서걸어가기_이석현.py
+++ lab/workspace/work_python/Day_1006/Main_9205_맥주마시면서걸어가기_이석현.py
@@ -1,20 +1,31 @@
 import sys
 input = sys.stdin.readline
-
+INF = int(1e9)
 # if abs(homeX-festivalX) + abs(homeY-festivalY) <= 1000:
 #     print('happy')
 #     continue
 
 for tc in range(int(input())):
     n = int(input())
-    homeX,homeY = map(int,input().split())
-    convenience = []
-    for _ in range(n):
-        convenience.append(list(map(int,input().split())))
-    convenience.sort(key=lambda x: (x[0],x[1]))
-    festivalX,festivalY = map(int,input().split())
+    graph = [[INF] * n for _ in range(n)]
 
+    for i in range(n+2):
+        graph[i][i] = 0
     
+    temp = []
+    for i in range(n+2):
+        pos = list(map(int, input().split()))
+
+        for j in range(len(temp)):
+            if abs(temp[j][0] - pos[0]) + abs(temp[j][1] - pos[1]) <= 1000:
+                graph[i][j] = graph[j][i] = 1
+        temp.append(pos)
+    
+    for k in range(n+2):
+        for i in range(n+2):
+            for j in range(n+2):
+                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+    print(graph[-1][-1])
     
         
     
