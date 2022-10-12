ea7e4854fdb8bc70e95cf07ebf58129992be00c0
leesh125 <chelsea12345@naver.com>
2022-10-13 01:00:49 +0900

    Day 57 과제

--- lab/workspace/work_python/Day_1011/Main_15961_회전초밥_이석현.py
+++ lab/workspace/work_python/Day_1011/Main_15961_회전초밥_이석현.py
@@ -2,6 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N,d,k,c = map(int, input().split())
+left, right = 0, k
 sushi_list = []
 for _ in range(N):
     sushi_list.append(int(input()))
@@ -13,24 +14,19 @@ for i in range(k):
     else:
         dict[sushi_list[i]] += 1
 ans = len(dict) if c in dict else len(dict) + 1
-print(dict)
-for i in range(1,N):
-    if dict[sushi_list[i-1]] == 1:
-        del dict[sushi_list[i-1]]
+
+while left < N:
+    if dict[sushi_list[left]] == 1:
+        del dict[sushi_list[left]]
     else:
-        dict[sushi_list[i-1]] -= 1
+        dict[sushi_list[left]] -= 1
     
-    if i >= N-k+1:
-        i -= (k+1)
-        if sushi_list[i] not in dict:
-            dict[sushi_list[i]] = 1
-        else:
-            dict[sushi_list[i]] += 1
+    if sushi_list[right % N] not in dict:
+        dict[sushi_list[right % N]] = 1
     else:
-        if sushi_list[i+k-1] not in dict:
-            dict[sushi_list[i+k-1]] = 1
-        else:
-            dict[sushi_list[i+k-1]] += 1
+        dict[sushi_list[right % N]] += 1
+    left += 1; right += 1
     size = len(dict) if c in dict else len(dict) + 1 
     ans = max(ans,size)
+
 print(ans)
