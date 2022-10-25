408a22b84a96572583222fb2a855453e5c63c2f2
leesh125 <chelsea12345@naver.com>
2022-10-25 17:41:49 +0900

    Day 67 Spring Mybatis, 동적 쿼리
    2022-10-25

---
+++ lab/workspace/work_spring/step05_mvc_mybatis2_noimpl_join_dq/src/main/java/com/ssafy/empapp/model/dto/Emp.java
@@ -0,0 +1,119 @@
+package com.ssafy.empapp.model.dto;
+
+public class Emp {
+	
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private String hiredate;	
+	private double sal;
+	private double comm;	
+	private int	deptno;
+	
+	private Dept dept;
+	
+	public Emp() {
+		super();
+	}
+
+	public Emp(int empno, String ename, String job, double sal, int deptno) {
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.sal = sal;
+		this.deptno = deptno;
+	}
+
+	public Emp(int empno, String ename, String job, int mgr, String hiredate, double sal, double comm, int deptno) {
+		super();
+		this.empno = empno;
+		this.ename = ename;
+		this.job = job;
+		this.mgr = mgr;
+		this.hiredate = hiredate;
+		this.sal = sal;
+		this.comm = comm;
+		this.deptno = deptno;
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public double getSal() {
+		return sal;
+	}
+
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+
+	public double getComm() {
+		return comm;
+	}
+
+	public void setComm(double comm) {
+		this.comm = comm;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public Dept getDept() {
+		return dept;
+	}
+
+	public void setDept(Dept dept) {
+		this.dept = dept;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", job=" + job + ", mgr=" + mgr + ", hiredate=" + hiredate
+				+ ", sal=" + sal + ", comm=" + comm + ", deptno=" + deptno + "]";
+	}	
+	
+	
+}
