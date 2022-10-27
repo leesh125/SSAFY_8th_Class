9fd315a6b3aef32a6facbe8a485810a1bac8132e
leesh125 <chelsea12345@naver.com>
2022-10-27 18:04:27 +0900

    Day 69 Spring Rest API, Transaction
    2022-10-27

--- lab/workspace/work_spring/step05_mvc_mybatis2_noimpl_join_dq/src/main/java/com/ssafy/empapp/model/service/DeptServiceImpl.java
+++ lab/workspace/work_spring/step05_mvc_mybatis2_noimpl_join_dq/src/main/java/com/ssafy/empapp/model/service/DeptServiceImpl.java
@@ -7,18 +7,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ssafy.empapp.model.dao.DeptDAO;
 import com.ssafy.empapp.model.dto.Dept;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Transactional
 @Slf4j
 @Service
 // Model : B/L
 public class DeptServiceImpl implements DeptService {
 
 	private DeptDAO deptDao;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	public DeptServiceImpl(DeptDAO deptDao) {
@@ -29,13 +32,14 @@ public class DeptServiceImpl implements DeptService {
 	@Override
 	public boolean registerDept(Dept dept) {
 		// 부서 등록
+//		return deptDao.insertDept(dept) > 0;
 		return deptDao.insertDept(dept) > 0;
 	}
 	@Override
 	public List<Dept> getDepts(){
 		
 		System.out.println("부서리스트 조회");
-		log.info("부서리스트 조회");
+		logger.info("부서리스트 조회");
 		
 		// 부서리스트 조회
 		return deptDao.selectDepts();
